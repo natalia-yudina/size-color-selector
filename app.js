7b8948c5b2327def1719e18ea912257d783d603a
natalia-yudina <yudina.nz@mail.ru>
2021-08-16 18:45:28 +1200

    using Babel and JSX
    index.html - include Babel in project
    app.js -  replaced all React.createElement to JSX expression

--- app.js
+++ app.js
@@ -4,34 +4,26 @@
   // ProductImage component
   // component starts with capital letter
   function ProductImage(props) {
-    // <img src="assets/red.jpg" alt="Product Image" />
-    return React.createElement("img", {
-      src: "assets/red.jpg",
-      width: "65%",
-      alt: "Product Image",
-    });
+    // JSX expression
+    return <img src="assets/red.jpg" width="65%" alt="Product Image" />;
   }
-
-  // //React.createElement(1 - what type of element we are creating, 2 - props to pass into the element, 3 - what should be inside element)
 
   // ProductCustomizer component
   function ProductCustomizer(props) {
-    return React.createElement(
-      "div",
-      { className: "customizer" },
-      // creating element for ProductImage component
-      React.createElement(
-        "div",
-        { className: "product-image" },
-        React.createElement(ProductImage)
-      )
+    return (
+      <div className="customizer">
+        <div className="product-image">
+          // capitalized
+          <ProductImage />
+        </div>
+      </div>
     );
   }
 
   // render components in the page (element to be rendered, place in html file - getElementById or querySelector)
   // create element which is represented by component ProductCustomizer and render it in browser
   ReactDOM.render(
-    React.createElement(ProductCustomizer),
+    <ProductCustomizer />,
     document.getElementById("react-root")
   );
 })();
