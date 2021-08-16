1b53ae3b68bfc53cd789e97a39b08350e3f92cd9
natalia-yudina <yudina.nz@mail.ru>
2021-08-16 16:21:42 +1200

    layout for the task

---
+++ app.js
@@ -0,0 +1,7 @@
+(function() {
+  "use strict";
+
+var ProductCustomizer = React.createElement('div', {className: "customizer"},
+"Product customizer will go here");
+ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+})();
