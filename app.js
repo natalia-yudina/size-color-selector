95a8e1382b5ca6e319bd7de689ceb9690f6d15ae
natalia-yudina <yudina.nz@mail.ru>
2021-08-16 17:43:51 +1200

    create a component by function

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 (function () {
   "use strict";
 
-  //React.createElement(1 - what type of element we are creating, 2 - props to pass into the element, 3 - what should be inside element)
-  var ProductCustomizer = React.createElement(
-    "div",
-    { className: "customizer" },
-    "Product customizer will go here"
-  );
+  // //React.createElement(1 - what type of element we are creating, 2 - props to pass into the element, 3 - what should be inside element)
+
+  // redefind element as a component
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      "Product customizer will go here"
+    );
+  }
 
   // render components in the page (element to be rendered, place in html file - getElementById or querySelector)
-  ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
+  // create element which is represented by component ProductCustomizer and render it in browser
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById("react-root")
+  );
 })();
 querySelector;
