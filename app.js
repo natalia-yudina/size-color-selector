51b9ed95f129de7e47fcd2768699e40b3495cbf1
natalia-yudina <yudina.nz@mail.ru>
2021-08-16 17:09:46 +1200

    render and createElement
    add React element to the site

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-(function() {
+(function () {
   "use strict";
 
-var ProductCustomizer = React.createElement('div', {className: "customizer"},
-"Product customizer will go here");
-ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+  //React.createElement(1 - what type of element we are creating, 2 - props to pass into the element, 3 - what should be inside element)
+  var ProductCustomizer = React.createElement(
+    "div",
+    { className: "customizer" },
+    "Product customizer will go here"
+  );
+
+  // render components in the page (element to be rendered, place in html file - getElementById or querySelector)
+  ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
 })();
+querySelector;
